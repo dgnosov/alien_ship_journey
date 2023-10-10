cb4a31f1ec6170f997c7dfaf8d0e3866ad4103ac
Daniel <ccsnosovdg@gmail.com>
2023-10-10 16:59:53 +0300

    feat: add shadows and lights

--- src/components/Ground.tsx
+++ src/components/Ground.tsx
@@ -9,11 +9,17 @@ const Ground: React.FC<Props> = ({}) => {
   });
 
   return (
-    <mesh receiveShadow>
-      <boxGeometry args={[100, 0.1, 100]} />
-      <meshStandardMaterial color={settings.color} />
-      <CuboidCollider args={[100, 0.1, 100]} />
-    </mesh>
+    <>
+      <mesh rotation={[-(Math.PI / 2), 0, 0]} position={[0, -0.1, 0]}>
+        <planeGeometry args={[30, 30, 30]} />
+        <meshStandardMaterial color={settings.color} />
+        {/* <CuboidCollider args={[100, 0.1, 100]} /> */}
+      </mesh>
+      <mesh position={[-4, 0.5, -4]} castShadow>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color={"green"} />
+      </mesh>
+    </>
   );
 };
 export default Ground;
